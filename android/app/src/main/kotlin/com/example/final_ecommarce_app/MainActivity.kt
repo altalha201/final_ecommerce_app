9e79d00d999ce2ddc313d54f5880a23194081a2b
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-04-27 20:04:12 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/final_ecommarce_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_ecommarce_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'ui/screens/splash_screen.dart';
+
+void main() {
+  runApp(const CraftyBay());
+}
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: "CraftyBay",
+      home: SplashScreen(),
+    );
+  }
+}
