406915ac3f99caf49a8b75ecf7eb907cd7cd42fa
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-04-27 21:45:00 +0600

    home screen nav_bar design

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widgets/nav_icon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset("assets/images/logo_nav.svg",),
+                  const Spacer(),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        NavIconWidget(icon: Icons.person_outline,),
+                        NavIconWidget(icon: Icons.call_outlined,),
+                        NavIconWidget(icon: Icons.notifications_active_outlined,),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
--- lib/ui/utils/application_colors.dart
+++ lib/ui/utils/application_colors.dart
@@ -2,3 +2,6 @@ import 'dart:ui';
 
 const Color colorPrimary = Color.fromRGBO(7, 174, 175, 1.0);
 const Color colorGray = Color.fromRGBO(152, 152, 153, 1.0);
+const Color colorGrayLite = Color.fromRGBO(243, 243, 243, 1.0);
+const Color navIconColor = Color.fromRGBO(158, 158, 158, 1.0);
+const Color colorWhite = Color.fromRGBO(254, 254, 254, 1.0);
