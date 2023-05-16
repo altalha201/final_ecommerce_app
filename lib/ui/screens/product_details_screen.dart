1a132056473fa38c46cd059ccc4626b1d4ea2fae
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-05-17 00:24:31 +0600

    product_details_screen design on going

--- lib/ui/screens/product_details_screen.dart
+++ lib/ui/screens/product_details_screen.dart
@@ -1,10 +1,131 @@
+import 'package:final_ecommerce_app/ui/utils/application_colors.dart';
+import 'package:final_ecommerce_app/ui/widgets/floating_price_widget.dart';
+import 'package:final_ecommerce_app/ui/widgets/item_stepper.dart';
+import 'package:final_ecommerce_app/ui/widgets/product_details_screen_widgets/product_image_carousel.dart';
+import 'package:final_ecommerce_app/ui/widgets/space.dart';
 import 'package:flutter/material.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({Key? key}) : super(key: key);
 
   @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Product Details"),
+        leading: const BackButton(
+          color: colorGray,
+        ),
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                ProductImageCarousel(),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Adidas Casual Shoe s4657 Brand new',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: appbarTextColor,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                verticalSpace(4.0),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Wrap(
+                                      crossAxisAlignment:
+                                          WrapCrossAlignment.center,
+                                      children: [
+                                        const Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                          size: 14,
+                                        ),
+                                        horizontalSpace(4.0),
+                                        const Text(
+                                          '4.5',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w500,
+                                              color: iconColor),
+                                        )
+                                      ],
+                                    ),
+                                    horizontalSpace(8.0),
+                                    TextButton(
+                                      onPressed: () {},
+                                      child: const Text(
+                                        "Reviews",
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: colorPrimary,
+                                        ),
+                                      ),
+                                    ),
+                                    horizontalSpace(8.0),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          color: colorPrimary,
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(4.0),
+                                        child: Icon(
+                                          Icons.favorite_border,
+                                          size: 16,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          ItemStepper(controller: TextEditingController()),
+                        ],
+                      ),
+                      verticalSpace(16.0),
+                      Text(
+                        "Colors",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: appbarTextColor.withOpacity(0.8),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          FloatingPriceWidget(
+            priceText: "1000",
+            onButtonTap: () {},
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/ui/widgets/product_details_screen_widgets/product_image_carousel.dart
@@ -0,0 +1,73 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:final_ecommerce_app/ui/utils/application_colors.dart';
+import 'package:flutter/material.dart';
+
+class ProductImageCarousel extends StatelessWidget {
+  ProductImageCarousel({Key? key}) : super(key: key);
+
+  final ValueNotifier<int> _currentIndex = ValueNotifier(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CarouselSlider(
+          items: [1, 2, 3, 4, 5].map((e) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "$e",
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                );
+              },
+            );
+          }).toList(),
+          options: CarouselOptions(
+              height: 200.0,
+              viewportFraction: 1,
+              autoPlay: true,
+              onPageChanged: (index, _) {
+                _currentIndex.value = index;
+              }),
+        ),
+        Positioned.fill(
+            child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: ValueListenableBuilder(
+              valueListenable: _currentIndex,
+              builder: (context, currentValue, _) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int i = 0; i < 5; i++)
+                      Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
+                          height: 10, width: 10,
+                          decoration: BoxDecoration(
+                            color: currentValue == i
+                                ? colorPrimary
+                                :colorWhite,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      )
+                  ],
+                );
+              },
+            ),
+          ),
+        )),
+      ],
+    );
+  }
+}
