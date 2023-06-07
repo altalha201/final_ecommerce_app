5b52cec2d26e40e2d38bf8c6b0aea176b5f5d7b0
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-06-08 00:04:56 +0600

    Brand List Screen

---
+++ lib/ui/screens/brand_list_screen.dart
@@ -0,0 +1,45 @@
+import 'package:final_ecommerce_app/ui/state_manager/list_states/brand_list_controller.dart';
+import 'package:final_ecommerce_app/ui/widgets/brand_card.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BrandListScreen extends StatelessWidget {
+  const BrandListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Brands"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GetBuilder<BrandListController>(
+          builder: (brandController) {
+            if (brandController.getBrandsInProgress) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return RefreshIndicator(
+              onRefresh: () async {
+                brandController.getBrands();
+              },
+              child: GridView.builder(
+                itemCount: brandController.brandListModel.brands?.length ?? 0,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 0,
+                      mainAxisSpacing: 0,
+                      childAspectRatio: 0.85),
+                  itemBuilder: (context, index) {
+                    return BrandCard(brandItem: brandController.brandListModel.brands![index]);
+                  }
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
